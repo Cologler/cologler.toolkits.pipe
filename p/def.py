30a810ad591bd1a63c269bbf567bb8cd80846790
赖巍 <skyoflw@gmail.com>
2017-02-06 15:38:18 +0800

    added default parser.

---
+++ p/def.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from ._common import ArgParser
+
+class DefaultParser(ArgParser):
+    pass
+
+export_parser = DefaultParser
